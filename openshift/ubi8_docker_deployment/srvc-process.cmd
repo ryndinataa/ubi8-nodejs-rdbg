72251a4ba4d3aea3c268accbc6d536922f24187c
Pavlo Shcherbukha <pavlo.shcherbukha@pravex.ua>
2023-03-26 21:52:53 +0300

    depl

--- openshift/ubi8_docker_deployment/srvc-process.cmd
+++ openshift/ubi8_docker_deployment/srvc-process.cmd
@@ -12,8 +12,10 @@ set GIT_BRANCH=tz-000001-init
 set GIT_URL=https://github.com/pavlo-shcherbukha/ubi8-nodejs-rdbg.git
 set DOCKER_PTH=./openshift/ubi8_docker_deployment/Dockerfile
 
-
+oc delete -f %fldepl%
+pause
 oc process -f %fltempl%  --param=NAMESPACE=%APP_PROJ% --param=APP_SERVICE_NAME=%APP_SERVICE_NAME% --param=APP_NAME=%APP_NAME% --param=GIT_BRANCH=%GIT_BRANCH% --param=GIT_URL=%GIT_URL% --param=DOCKER_PTH=%DOCKER_PTH% -o yaml > %fldepl% 
+pause
 oc create -f %fldepl%
 pause
 
