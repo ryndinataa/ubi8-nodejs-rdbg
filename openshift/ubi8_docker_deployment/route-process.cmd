aeb744bc74169be87a6122b68fe443ba659d89c6
Pavlo Shcherbukha <pavlo.shcherbukha@pravex.ua>
2023-03-26 21:27:12 +0300

    depl

---
+++ openshift/ubi8_docker_deployment/route-process.cmd
@@ -0,0 +1,42 @@
+﻿@echo on
+call login.cmd
+oc project %APP_PROJ%
+pause
+
+set fltempl=nodejs-s2i-route-temp.yaml
+set fldepl=nodejs-s2i-route-depl.yaml
+ 
+
+echo %APP_PROJ% .....
+
+echo %APP_DNS%   .....
+
+set APP_SERVICE_NAME=nodejs-s2i-srvc
+set APP_NAME=nodejs-remote-debug
+set ROUTENAME=nodes2i-srvc-%APP_PROJ%.%APP_DNS%
+set SRVHOSTNAME=nodes2i-srvc-%APP_PROJ%.%APP_DNS%
+set PORTNUMBER=8080
+
+
+echo APP_SERVICE_NAME=%APP_SERVICE_NAME%
+echo APP_NAME=%APP_NAME%
+echo ROUTENAME=%ROUTENAME%
+echo SRVHOSTNAME=%SRVHOSTNAME%
+echo PORTNUMBER=%PORTNUMBER%
+
+
+pause
+
+
+
+oc delete -f %fldepl%
+
+pause
+oc process -f %fltempl%  --param=NAMESPACE=%APP_PROJ%  --param=APP_SERVICE_NAME=%APP_SERVICE_NAME% --param=APP_NAME=%APP_NAME% --param=ROUTENAME=%ROUTENAME% --param=HOSTNAME=%SRVHOSTNAME% --param=PORTNUMBER=%PORTNUMBER% -o yaml > %fldepl% 
+pause
+oc create -f %fldepl%
+pause
+ 
+     
+     
+     
---
+++ openshift/ubi8_docker_deployment/srvc-process.cmd
@@ -0,0 +1,20 @@
+﻿@echo off
+call ..\login.cmd
+oc project %APP_PROJ%
+pause
+
+set fltempl=nodejs-s2i-srvc-templ.yaml
+set fldepl=nodejs-s2i-srvc-depl.yaml 
+oc delete -f %fldepl%
+set APP_SERVICE_NAME=nodejs-s2i-srvc
+set APP_NAME=nodejs-remote-debug
+set GIT_BRANCH=tz-000001-init
+set GIT_URL=https://github.com/pavlo-shcherbukha/ubi8-nodejs-rdbg.git
+set DOCKER_PTH=/openshift/ubi8_docker_deployment/Dockerfile
+
+
+oc process -f %fltempl%  --param=NAMESPACE=%APP_PROJ% --param=APP_SERVICE_NAME=%APP_SERVICE_NAME% --param=APP_NAME=%APP_NAME% --param=GIT_BRANCH=%GIT_BRANCH% --param=GIT_URL=%GIT_URL% --param=DOCKER_PTH=%DOCKER_PTH% -o yaml > %fldepl% 
+oc create -f %fldepl%
+pause
+
+ 
