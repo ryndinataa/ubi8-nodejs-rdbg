d8a3b7b0ecfbc86ca8c9a30eb83e52a6ef355f34
Pavlo Shcherbukha <pavlo.shcherbukha@pravex.ua>
2023-03-27 11:56:21 +0300

    aldoc

--- nodesrvcbe/apprun.sh
+++ nodesrvcbe/apprun.sh
@@ -1,13 +1,14 @@
-﻿if [ -z "$DEBUG_PORT" ]
-then
-  export DEBUG_PORT=5858
-fi
+﻿#!/bin/bash
 
-if [ -z "$NPM_RUN" ]
-then
-  export NPM_RUN=start
-fi
+run_node() {
+  if [ -z "$DEBUG_PORT" ]; then
+     export DEBUG_PORT=5858
+  fi
 
-
+  if [ -z "$NPM_RUN" ]; then
+     export NPM_RUN=start
+  fi
+}
+run_node
 echo "Launching via npm..."
- exec npm run -d $NPM_RUN
+exec npm run -d $NPM_RUN
